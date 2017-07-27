70ef332be7faa70f1aae1a103452b568f3db6c29
axou <axel.adainejeanpierre@viacesi.fr>
2017-07-27 15:12:48 +0200

    creation back

---
+++ back/src/main/java/com/team/DeskPosable/DeskPosableApplication.java
@@ -0,0 +1,12 @@
+package com.team.DeskPosable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskPosableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskPosableApplication.class, args);
+	}
+}
