9ae73e9371adf8355b5eca632cd7a95ce114a503
Marc ETOURNEAU <marc.etourneau@gmail.com>
2017-07-27 17:16:10 +0200

    mo

--- back/src/main/java/com/team/DeskPosable/Entity/Building.java
+++ back/src/main/java/com/team/DeskPosable/Entity/Building.java
@@ -1,6 +1,9 @@
 package com.team.DeskPosable.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by Marc ETOURNEAU on 27/07/2017.
@@ -12,5 +15,10 @@ public class Building {
     @Id
     @GeneratedValue
     private Long id;
+    private String label;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "building")
+    private List<Map> maps;
 
 }
