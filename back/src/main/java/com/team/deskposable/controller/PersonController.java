05d7d41bc75e6636df6d19247e61b18be7da1d33
azawialoff <alexiszaw@gmail.com>
2017-07-28 10:12:35 +0200

    Création du controller, de l'entité et du repository de Person

---
+++ back/src/main/java/com/team/deskposable/controller/PersonController.java
@@ -0,0 +1,63 @@
+package com.team.deskposable.controller;
+
+import com.team.deskposable.entity.Person;
+import com.team.deskposable.repository.PersonRepository;
+import com.team.deskposable.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alec_ on 28/07/2017.
+ */
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @PostMapping
+    public @ResponseBody
+    String addNewPerson(@RequestBody Person person) {
+        if(person != null){
+            personRepository.save(person);
+            return "ok";
+        }
+        return "errors";
+    }
+
+    @GetMapping("/all")
+    public ArrayList<Person> getAllPerson() {
+        return (ArrayList<Person>) personRepository.findAll();
+    }
+
+    @GetMapping("/id")
+    public Person getPerson (@PathVariable Long id) {
+        return personRepository.findOne(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletePerson(@PathVariable Long id) {
+        personRepository.delete(id);
+        return "ok";
+    }
+
+    @PutMapping("/{id}")
+    public String modifyPerson(@PathVariable Long id, @RequestBody Person person) {
+        Person modifyPerson = personRepository.findOne(id);
+        modifyPerson.setFirstname(person.getFirstname());
+        modifyPerson.setLastname(person.getLastname());
+        modifyPerson.setEmployment(person.getEmployment());
+        modifyPerson.setDesk(person.getDesk());
+        personRepository.save(modifyPerson);
+        return "ok";
+    }
+
+
+}
+
+
+
---
+++ back/src/main/java/com/team/deskposable/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.team.deskposable.repository;
+
+import com.team.deskposable.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by alec_ on 28/07/2017.
+ */
+public interface PersonRepository extends CrudRepository<Person, Long> {
+}
