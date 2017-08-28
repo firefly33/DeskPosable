288c5c2de132b401f7f6db53535eea7e763b914f
Marc ETOURNEAU <marc.etourneau@gmail.com>
2017-08-28 14:13:15 +0200

    ajout d'une ligne

--- back/src/main/java/com/team/deskposable/controller/DeskController.java
+++ back/src/main/java/com/team/deskposable/controller/DeskController.java
@@ -1,7 +1,9 @@
 package com.team.deskposable.controller;
 
 import com.team.deskposable.entity.Desk;
+import com.team.deskposable.entity.Map;
 import com.team.deskposable.repository.DeskRepository;
+import com.team.deskposable.repository.MapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,8 @@ public class DeskController {
 
     @Autowired
     DeskRepository deskRepository;
+    @Autowired
+    MapRepository mapRepository;
 
     @GetMapping()
     public Iterable<Desk> desks() { return deskRepository.findAll(); }
@@ -50,5 +54,19 @@ public class DeskController {
         deskRepository.save(deskToEdit);
 
         return deskToEdit;
+    }
+
+    @PutMapping("/{id}")
+    public Map addDesk(@PathVariable Long id, @RequestBody Desk desk) {
+        Map mapToEdit = mapRepository.findOne(id);
+
+        if (mapToEdit != null) {
+            mapToEdit.setLabel(mapToEdit.getLabel());
+            mapToEdit.setImagePath(mapToEdit.getImagePath());
+            mapToEdit.setBuilding(mapToEdit.getBuilding());
+            mapRepository.save(mapToEdit);
+        }
+
+        return mapToEdit;
     }
 }
