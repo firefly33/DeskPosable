6737a905191a36296340365f32dd555ee541cc3d
azawialoff <alexiszaw@gmail.com>
2017-08-30 10:28:19 +0200

    Implémentation de l'angular JS plus intégration de quelques exemples.

--- back/src/main/java/com/team/deskposable/controller/UserController.java
+++ back/src/main/java/com/team/deskposable/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/usersControllers")
 public class UserController {
 
     @Autowired
